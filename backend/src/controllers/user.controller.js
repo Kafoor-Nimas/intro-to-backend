8443612ba38717728a37d3e1aed47390de3233ae
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-13 20:48:41 +0530

    created user controller for register user

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,31 @@
+import User from "../models/user.models.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    //basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are important" });
+    }
+    const existing = await User.findOne({ email: email.toLowerCase() });
+    if (existing) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password,
+      loggedIn: false,
+    });
+    res.status(201).json({
+      message: "User registered",
+      user: { id: user._id, email: user.email, username: user.username },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export { registerUser };
